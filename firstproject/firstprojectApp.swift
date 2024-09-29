5c7e2e3bf6764e1710256286773f6babad2c7602
irah2f <rahafghamdi02@gmail.com>
2024-09-29 10:28:58 +0300

    Initial Commit

---
+++ firstproject/firstprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstprojectApp.swift
+//  firstproject
+//
+//  Created by Rahaf Khaled Krat on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct firstprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ firstprojectUITests/firstprojectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  firstprojectUITestsLaunchTests.swift
+//  firstprojectUITests
+//
+//  Created by Rahaf Khaled Krat on 26/03/1446 AH.
+//
+
+import XCTest
+
+final class firstprojectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
